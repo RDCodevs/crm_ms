58d5628d9e3ccf5d6e5cfe5326853c843c2c123c
JEDAV01 <136944117+JEDAV01@users.noreply.github.com>
2023-06-25 22:20:38 -0500

    Conexión a la base de datos

--- src/config/database.py
+++ src/config/database.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 sqlite_file = "../database.sqlite"
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
+
 database_url = f'sqlite:///{os.path.join(base_dir, sqlite_file)}'
 
 engine = create_engine(database_url, echo= True)
